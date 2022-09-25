025a99c4b5ce402602e08e4d312e071d9bb11183
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2022-09-25 03:47:09 -0400

    found problem

--- issue.js
+++ issue.js
@@ -4,7 +4,8 @@ const findFreePort = require('find-free-port')
 const PORT_LOWER = 9000
 const PORT_UPPER = 9010
 
-findFreePort(PORT_LOWER, PORT_UPPER,  function(err, freePort){
+// findFreePort(PORT_LOWER, PORT_UPPER, function(err, freePort){
+findFreePort(PORT_LOWER, PORT_UPPER, '127.0.0.1', function(err, freePort){
   if (err) {
     console.log(err)
     console.log(`Cannot find free port between ${PORT_LOWER}, ${PORT_UPPER}`)
